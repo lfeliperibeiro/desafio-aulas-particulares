ef5c5232e66b9696f2d94827a0a8bfbbd3c5de42
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-06-26 00:24:07 -0300

    update
    Criação de rotas, servidor e formulário

--- routes.js
+++ routes.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const routes = express.Router();
+const teachers = require("./teachers");
+const { response } = require("express");
 
-routes.get("/", function (require, response) {
-  return response.redirect("/teachers");
+routes.get("/", function (req, res) {
+  return res.redirect("/teachers");
 });
 
-routes.get("/teachers", function (require, response) {
-  return response.render("teachers/index");
+routes.get("/teachers", function (req, res) {
+  return res.render("teachers/index");
 });
 
-routes.get("/students", function (require, response) {
-  return response.render("students");
+routes.get("/teachers/create", function (req, res) {
+  return res.render("teachers/create");
+});
+
+routes.post("/teachers", teachers.post);
+
+routes.get("/students", function (req, res) {
+  return res.render("students");
 });
 
 module.exports = routes;
