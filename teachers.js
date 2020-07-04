5d2279864f965c8043111d009a4d58441a7ef418
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-07-04 00:25:19 -0300

    update
    lógica para imprimir o select em tela e ajustar a data para os padrões brasileiros e idade

--- teachers.js
+++ teachers.js
@@ -1,37 +1,7 @@
 const fs = require("fs");
 const data = require("./data.json");
-const { age } = require("./utils");
-
-exports.post = function (req, res) {
-  const keys = Object.keys(req.body);
-  for (key of keys) {
-    if (req.body[key] == "") {
-      return res.send("Preencha os campos vazios");
-    }
-  }
-
-  let { avatar_url, name, birth, formation, classes, services } = req.body;
-  birth = Date.parse(birth);
-  const create_at = Date.now();
-  const id = Number(data.teachers.length + 1);
-
-  data.teachers.push({
-    id,
-    avatar_url,
-    name,
-    birth,
-    formation,
-    classes,
-    services,
-    create_at,
-  });
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send("Erro na escrita do arquivo");
-
-    return res.redirect("/teachers");
-  });
-};
+const { age, graduation } = require("./utils");
+const Intl = require("intl");
 
 exports.show = function (req, res) {
   const { id } = req.params;
@@ -43,8 +13,40 @@ exports.show = function (req, res) {
   const teacher = {
     ...foundTeacher,
     birth: age(foundTeacher.birth),
-    services: "",
-    create_at: "",
+    services: foundTeacher.services.split(", "),
+    formation: graduation(foundTeacher.formations),
+    created_at: new Intl.DateTimeFormat("pt-br").format(foundTeacher.create_at),
   };
   return res.render("teachers/show", { teacher });
+};
+
+exports.post = function (req, res) {
+  const keys = Object.keys(req.body);
+  for (key of keys) {
+    if (req.body[key] == "") {
+      return res.send("Preencha os campos vazios");
+    }
+  }
+
+  let { avatar_url, name, birth, formations, classes, services } = req.body;
+  birth = Date.parse(birth);
+  const created_at = Date.now();
+  const id = Number(data.teachers.length + 1);
+
+  data.teachers.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    formations,
+    classes,
+    services,
+    created_at,
+  });
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("Erro na escrita do arquivo");
+
+    return res.redirect("/teachers");
+  });
 };
--- utils.js
+++ utils.js
@@ -1,14 +1,26 @@
 module.exports = {
   age: function age(timestamp) {
     const today = new Date();
-    const birth = new Date(timestamp);
+    const birthDate = new Date(timestamp);
 
-    let age = today.getFullYear() - birth.getFullYear();
-    const month = today.getMonth() - birth.getMonth();
-
-    if (month < 0 || (month == 0 && today.getDate() <= birth.getDate())) {
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month == 0 && today.getDate() <= birthDate.getDate())) {
       age = age - 1;
     }
-    return age
+    return age;
+  },
+  graduation: function formations(education) {
+    const formantions = [
+      "Ensino médio Completo",
+      "Ensino Superior Completo",
+      "Mestrado",
+      "Doutorado",
+    ];
+    for (let formation of formantions) {
+      if (education == [formation]) {
+        return formations;
+      }
+    }
   },
 };
