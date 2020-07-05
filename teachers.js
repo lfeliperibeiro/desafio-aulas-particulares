b41bc5c5db348d58de036eb3c9ad8d2df016b364
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-07-04 23:34:16 -0300

    update
    ajustes do css para o botão deletar ficar ao lado do salvar

--- teachers.js
+++ teachers.js
@@ -89,3 +89,18 @@ exports.put = function (req, res) {
     return res.redirect(`/teachers/${id}`)
   })
 };
+
+exports.delete = function(req, res){
+  const {id} = req.body
+  const filteredTeachers = data.teachers.filter(function(teacher){
+    return teacher.id != id
+  })
+
+  data.teachers = filteredTeachers
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err)=>{
+    if(err) return res.send('Professor não encontrado')
+
+    return res.redirect('/teachers')
+  })
+}
