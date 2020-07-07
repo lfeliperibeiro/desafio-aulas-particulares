7a215e4c4a1a3f694189fdc995ba2b21d67daca0
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-07-06 22:00:55 -0300

    create page students

--- controllers/students.js
+++ controllers/students.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
-const data = require("./data.json");
-const { age, graduation, date } = require("./utils");
+const data = require("../data.json");
+const { age, graduation, date } = require("../utils");
 const Intl = require("intl");
 
 exports.index = function (req, res) {
-  return res.render("teachers/index", {teachers: data.teachers});
-}
+  return res.render("teachers/index", { teachers: data.teachers });
+};
+
+exports.create = function (req, res) {
+  return res.render("teachers/create");
+};
 
 exports.show = function (req, res) {
   const { id } = req.params;
@@ -84,28 +88,28 @@ exports.put = function (req, res) {
     ...foundTeacher,
     ...req.body,
     birth: Date.parse(req.body.birth),
-    id: Number(req.body.id)
+    id: Number(req.body.id),
   };
-  data.teachers[index] = teacher
+  data.teachers[index] = teacher;
 
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), (err)=>{
-    if(err) return res.send('Professor não encontrado')
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("Professor não encontrado");
 
-    return res.redirect(`/teachers/${id}`)
-  })
+    return res.redirect(`/teachers/${id}`);
+  });
 };
 
-exports.delete = function(req, res){
-  const {id} = req.body
-  const filteredTeachers = data.teachers.filter(function(teacher){
-    return teacher.id != id
-  })
+exports.delete = function (req, res) {
+  const { id } = req.body;
+  const filteredTeachers = data.teachers.filter(function (teacher) {
+    return teacher.id != id;
+  });
 
-  data.teachers = filteredTeachers
+  data.teachers = filteredTeachers;
 
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err)=>{
-    if(err) return res.send('Professor não encontrado')
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("Professor não encontrado");
 
-    return res.redirect('/teachers')
-  })
-}
+    return res.redirect("/teachers");
+  });
+};
